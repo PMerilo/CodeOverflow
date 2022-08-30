0b5ca8f479c624e20b7e1be0c72ad88979d5ee88
Perry M <pgmerilo@gmail.com>
2022-08-30 20:01:57 +0800

    Test

--- config/DBConnection.js
+++ config/DBConnection.js
@@ -1,5 +1,7 @@
 const mySQLDB = require('./DBConfig');
-
+const Chat = require('../models/Chat');
+const User = require('../models/User');
+const Msg = require('../models/Msg');
 // If drop is true, all existing tables are dropped and recreated
 const setUpDB = (drop) => {
     mySQLDB.authenticate()
@@ -9,7 +11,15 @@ const setUpDB = (drop) => {
             Defines the relationship where a user has many videos.
             The primary key from user will be a foreign key in video.
             */
-            
+            Chat.belongsTo(User, {as: "buyer", foreignKey: 'buyerId'} )
+            User.hasMany(Chat)
+
+            Msg.belongsTo(Chat)
+            Chat.hasMany(Msg)
+
+            Msg.belongsTo(User)
+            User.hasMany(Msg)
+
             mySQLDB.sync({
                 alter: true,
                 force: drop
